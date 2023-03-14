54259706be77d1c45d88c5b30f9fa4ee38504106
Hoon Kim <eloael@outlook.com>
2023-03-13 21:01:44 -0700

    added client side starter code

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+
+
+
+
+
+export default App;
