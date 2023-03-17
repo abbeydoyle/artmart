af8486d753c4f5082ec7648ee8bbff5af0e7f25e
Faruk Ajiya <Ajiyafaruk@gmail.com>
2023-03-16 21:49:16 -0700

    utils completed

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,80 @@
+import { useReducer } from "react";
+import {
+  ADD_TO_CART,
+  UPDATE_CART_QUANTITY,
+  REMOVE_FROM_CART,
+  CLEAR_CART,
+  TOGGLE_CART,
+  ADD_TO_WISHLIST,
+  REMOVE_FROM_WISHLIST,
+} from "./actions";
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
+
+    case ADD_TO_WISHLIST:
+      return {
+        ...state,
+        wishlistOpen: true,
+        wishlist: [...state.wishlist, action.product],
+      };
+
+    case REMOVE_FROM_WISHLIST:
+      let newwishlistState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+      return {
+        ...state,
+        wishlistOpen: newwishlistState.length > 0,
+        wishlist: newwishlistState,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export function useProductReducer(initialState) {
+  return useReducer(reducer, initialState);
+}
