42c0e0028223317f8e53703f7b27670a7f26f907
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-03-20 19:25:49 -0700

    Still working on it

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -22,35 +22,35 @@ function ProductItem(item) {
     const { cart } = state
 
   //TODO: we probably don't need addToCart here because you won't be able to add to cart straight from homepage without going to the artwork?
-    const addToCart = () => {
-      const itemInCart = cart.find((cartItem) => cartItem._id === _id)
-      if (itemInCart) {
-        dispatch({
-          type: UPDATE_CART_QUANTITY,
-          _id: _id,
-          purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-        });
-        idbPromise('cart', 'put', {
-          ...itemInCart,
-          purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-        });
-      } else {
-        dispatch({
-          type: ADD_TO_CART,
-          product: { ...item, purchaseQuantity: 1 }
-        });
-        idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
-      }
-    }
+    // const addToCart = () => {
+    //   const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    //   if (itemInCart) {
+    //     dispatch({
+    //       type: UPDATE_CART_QUANTITY,
+    //       _id: _id,
+    //       purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+    //     });
+    //     idbPromise('cart', 'put', {
+    //       ...itemInCart,
+    //       purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+    //     });
+    //   } else {
+    //     dispatch({
+    //       type: ADD_TO_CART,
+    //       product: { ...item, purchaseQuantity: 1 }
+    //     });
+    //     idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+    //   }
+    // }
 
   return (
     <div className="">
         <p>ProductItem comp</p>
       <Link to={`/products/${_id}`}>
-        <p>plz work</p>
+        {/* <p>plz work</p> */}
         <img alt={name} src={`${image}`}/>
-        <p>{name}</p>
-        <p>{artistName}</p>
+        <p>Name{name}</p>
+        <p>Artist{artistName}</p>
       </Link>
     </div>
   );
