30293fe571ac32cf28ef4e3ab5539e99e6a2ff93
Abigail Doyle <abigaildoyle3@gmail.com>
2023-03-22 21:51:48 -0700

    react mode

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,9 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // react strict mode has been disabled for stripe
+  <React.StrictMode>
     <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
