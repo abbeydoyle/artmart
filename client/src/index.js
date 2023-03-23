5f75333dfa9c8ea9ff2716f1fe5715757616b770
Abigail Doyle <abigaildoyle3@gmail.com>
2023-03-22 21:11:26 -0700

    not a pwa

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,3 +15,9 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
